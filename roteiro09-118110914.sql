fef83a575640250f8c06ba42bae48a15d662360e
marques596 <40725249+marques596@users.noreply.github.com>
2019-12-01 23:03:44 -0300

    roteiro 9 finalizado

--- roteiro09-118110914.sql
+++ roteiro09-118110914.sql
@@ -21,59 +21,65 @@ AS SELECT pno AS projetoID, count(essn) AS quant_func
 FROM works_on
 GROUP BY pno;
 
--- questao 4 
+-- Funcao do roteiro 9 - questao 2
 CREATE FUNCTION check_age(pssn char(9))
-RETURNS char(20) AS
-BEGIN
-	DECLARE 
+RETURNS VARCHAR(10) AS
+$$
+
+DECLARE
 	idade integer;
-	retorno char(20);
- 
-	SELECT extract(year from age(bdate)) INTO idade FROM employee as e WHERE e.ssn = pssn
-
-	print(idade)
-
-		IF idade > 50 BEGIN
-		SET retorno = 'SENIOR'
-		END
-
-		ELSE IF idade < 50 BEGIN
-		SET retorno = 'YOUNG'
-		END
-		
-		ELSE IF idade IS NULL BEGIN
-		SET retorno = 'YOUNG'
-		END
-
-		ELSE IF idade < 0 BEGIN
-		SET retorno = 'INVALID'
-		END
-	RETURN retorno
-END;
-
-CREATE FUNCTION check_age(pssn char(9))
-RETURNS char(20) AS $$
-
-DECLARE 
-idade integer;
 
 BEGIN
- 
-SELECT extract(year from age(bdate)) INTO idade FROM employee as e WHERE e.ssn = pssn;
+	SELECT extract(year FROM age(bdate)) INTO idade FROM employee WHERE ssn = pssn;
+	RAISE NOTICE 'Idade: %', idade;
+	
+	IF( idade >= 50) THEN RETURN 'SENIOR';
+	ELSEIF(idade <= 0) THEN RETURN 'INVALID';
+	ELSEIF(idade <= 50) THEN RETURN 'YOUNG';
+	ELSEIF(idade is NULL) THEN RETURN 'UNKNOWN';
 
-
-	
-	IF (idade > 50) THEN RETURN 'SENIOR'
-	
-	
-	ELSIF (idade < 50) THEN RETURN 'YOUNG'
-	
-	
-	ELSIF (idade IS NULL) THEN RETURN 'YOUNG'
-	
-	
-	ELSIF (idade < 0) THEN RETURN 'INVALID'
 	END IF;
+	
 
 END;
+
+
 $$ LANGUAGE plpgsql;
+
+
+-- roteiro 9 -- questao 3
+
+CREATE FUNCTION check_mgr() RETURNS TRIGGER
+AS
+$$
+	DECLARE
+		mgr_ssn CHAR(9);
+		idade_status INTEGER;
+
+	BEGIN 
+		idade_status := SELECT check_age(NEW.mgrssn);
+
+		IF NOT EXISTS (SELECT e.ssn FROM employee AS e, department as d WHERE (NEW.mgrssn = e.ssn and e.dno = d.dnumber) ) 
+		THEN RAISE EXCEPTION 'manager must be a departments employee';
+		END IF;
+
+		IF NOT EXISTS (SELECT e.ssn FROM employee AS e WHERE (NEW.mgrssn = e.superssn) ) 
+		THEN RAISE EXCEPTION 'manager must have supervisees';
+		END IF;
+
+		IF (idade_status != 'SENIOR') THEN RAISE EXCEPTION 'manager must be a SENIOR employee';
+		END IF;
+
+
+	END;
+
+$$ LANGUAGE plpgsql;
+
+
+
+CREATE TRIGGER check_mgr
+BEFORE INSERT ON department
+
+FOR EACH ROW EXECUTE PROCEDURE check_mgr();
+
+
